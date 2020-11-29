866bcb2903e1b7ae1ec2c38c62f82602c04fe0fe
KrishAGARWA <73591403+KrishAGARWA@users.noreply.github.com>
2020-11-29 01:04:45 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,29 @@
+class Box{
+constructor(x,y,width,height){
+   
+ this.body=Bodies.rectangle(x,y,width,height
+)
+ 
+ this.width=width
+ this.height=height
+ 
+
+World.add(world,this.body)
+
+
+}
+display(){
+    fill("red")
+var pos=this.body.position
+rectMode(CENTER)
+rect(pos.x,pos.y,this.width,this.height)
+
+
+
+
+}
+
+
+
+
+}
